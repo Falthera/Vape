d0a95009f84c8199fc3da2ab5f402bb23d98967f
Falthera <aaryadevv8@gmail.com>
2026-05-08 21:13:22 +1000

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,9 @@
 @echo off
-gradle %*
+setlocal
+
+set "APP_HOME=%~dp0"
+set "WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
+
+java -classpath "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
+
+endlocal
